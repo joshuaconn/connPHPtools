835bbe55d7e686f78d0fb4dbd2df66c5fd346fdd
Joshua Conn <joshuadavidconn@gmail.com>
2014-10-06 23:03:03 -0400

    light error detection in show_src.php

--- show_src.php
+++ show_src.php
@@ -4,19 +4,29 @@
  *
  * Renders the source code of and internal file to the browser
  * @var $_GET['path']
+ * @author Joshua Conn
  */
 	$path=$_GET['path'];
+	$contents = file_get_contents($path);
 ?>
 <html>
 <body>
-<h1>Source of <?=html_out($path)?></h1>
+<h1>Source of <?=htmlentities($path)?></h1>
+<?php
+	if($contents!==FALSE) {
+	?>
 	<div style="background-color:#ddd;">
 		<code>
 			<?=nl2br(
-				str_replace(array(' ',"\t"),array('&nbsp;','&nbsp; &nbsp; &nbsp;'),html_out(
-						file_get_contents($path)
+				str_replace(array(' ',"\t"),array('&nbsp;','&nbsp; &nbsp; &nbsp;'),htmlentities(
+						$contents
 				)))?>
 		</code>
 	</div>
+	<?php } else { ?>
+	<div style="background-color:#faa;">
+		Cannot display the contents of this file.
+	</div>
+	<?php } ?>
 </body>
 </html>
