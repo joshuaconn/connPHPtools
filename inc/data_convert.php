e338a62e15557f322db8c08ff9b36923f7f99e37
Joshua Conn <joshuadavidconn@gmail.com>
2014-10-06 21:39:50 -0400

    Create data_convert.php
    Just contains bool2str() for now

---
+++ inc/data_convert.php
@@ -0,0 +1,20 @@
+/**
+ * data_convery.php
+ * Basic data conversion functions
+ * @author Joshua David Conn
+ */
+
+/**
+ * Handy way to rener a boolean value as a string inline
+ * @param boolean $condtion condition to test
+ * @param string $true	what to return if $condtion is true
+ * @param string $false	what to return if $condtion is false
+ */
+function bool2str($condtion, $true='TRUE', $false='FALSE')
+{
+	if($condtion)
+	{
+		return $true;
+	}
+	return $false;
+}
