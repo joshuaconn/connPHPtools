14961024bac62e1f5c1e4ff63c5b6024f9dff795
Joshua Conn <joshuadavidconn@gmail.com>
2015-03-20 00:00:46 -0400

    Update dump_state.php: defined vars was not useful
    get_defined_vars() only returns local variables, which are useless for the purpose of this function

--- dump_state.php
+++ dump_state.php
@@ -1,11 +1,11 @@
 <?php
 /**
   * dumps defined variables, constants, and a backtrace to the browser
+  * @example dump_state(get_defined_vars());
   */
-function dump_state()
+function dump_state($defined_vars)
 {
 	echo "<h1>defined vars:</h1>\n";
-	$defined_vars = get_defined_vars();
 	var_dump($defined_vars);
 	echo "\n<h1>defined constants:</h1>\n";
 	$defined_constants = get_defined_constants ();
