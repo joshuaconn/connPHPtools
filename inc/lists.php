38ffa0d01b5fccfb5f5f01f2e902554889f264d5
Joshua Conn <joshuadavidconn@gmail.com>
2015-12-14 18:35:22 -0500

    Update lists.php
    Correction in whitespace

--- inc/lists.php
+++ inc/lists.php
@@ -6,14 +6,14 @@
 
 /**
  *	Test whether every item in a list passes a test. 
- *  Inspired by all() in Python
+ *	Inspired by all() in Python
  *	@param array $list values to be tested
  *	@param callback $tester_callback a function that accepts one parameter and
  *		returns true or false. If null, list items are evaluated directly.
- *  @example every(array('1',"2"),'is_string'); //returns true
- *  @example every(array('1',"2",3),'is_string'); //returns false
+ *	@example every(array('1',"2"),'is_string'); //returns true
+ *	@example every(array('1',"2",3),'is_string'); //returns false
  *	@return boolean
- *  @see any()
+ *	@see any()
  */
 function every(array $list, $tester_callback=NULL)
 {
@@ -34,14 +34,14 @@ function every(array $list, $tester_callback=NULL)
 
 /**
  *	Test whether any item in a list passes a test.
- *  Inspired by any() in Python
+ *	Inspired by any() in Python
  *	@param array $list values to be tested
  *	@param callback $tester_callback a function that accepts one parameter and
  *		returns true or false.
- *  @example any(array('1',"2",3),'is_string'); //returns true
- *  @example any(array(1,2,3),'is_string'); //returns false
+ *	@example any(array('1',"2",3),'is_string'); //returns true
+ *	@example any(array(1,2,3),'is_string'); //returns false
  *	@return boolean
- *  @see every()
+ *	@see every()
  */
 function any(array $list, $tester_callback=NULL)
 {
