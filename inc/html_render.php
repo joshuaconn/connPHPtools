761c27244aec977fb480f9306878970ea664128d
Joshua Conn <joshuadavidconn@gmail.com>
2016-04-22 01:44:19 -0400

    adding html_render.php
    beginning of library of generic PHP functions for rendering HTML

---
+++ inc/html_render.php
@@ -0,0 +1,21 @@
+/**
+ * Turns an array into an HTML seletion box for use in a form
+ * @param $options keys are values for the option elements; values are what gets displayed
+ * @param $default if this matches one of the $options keys, that is selected by default
+ * @param $id the HTML id and name used for the selection box rendered
+ * @return string the rendered HTML
+ */
+function disp_options(array $options,$default=null,$id='')
+{
+	if($id!='')
+	{
+		$id=" id='$id' name='$id'";
+	}
+	$retval = "<select$id>";
+	foreach ($options as $n=>$v)
+	{
+		$retval.='<option value="'.html_out($n).'"'.($n==$default?' selected':'').'>'.
+			html_out($v).'</option>';
+	}
+	return  $retval."</select>";
+}
